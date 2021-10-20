49a7f5970b081c6f79cd6c11f640c202b5fd06a3
zzzdnwo <swjdnwo@gmail.com>
2021-10-20 17:46:35 +0900

    1020clock

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date(),
-    hours
-    // 다시 변수 선언해서 padStart먹이자
+        hours = String(date.getHours()).padStart(2,"0"),
+        minutes = String(date.getMinutes()).padStart(2,"0"),
+        seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 getClock();
 setInterval(getClock,1000)
